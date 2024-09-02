b3b57e95e420d68c69dc6392e4ee50f73faa05a6
Mohamed Ogleh <mohamedogleh0@gmail.com>
2024-09-02 17:56:58 +0300

    greeting endpoint added

--- src/main/java/org/ogleh/mytestapp/MyTestAppApplication.java
+++ src/main/java/org/ogleh/mytestapp/MyTestAppApplication.java
@@ -3,6 +3,7 @@ package org.ogleh.mytestapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,7 +15,12 @@ public class MyTestAppApplication {
     }
 
     @GetMapping("/")
-    public String greeting(){
+    public String greeting() {
         return "Hello World!";
+    }
+
+    @GetMapping("/name")
+    public String greeting(@PathVariable("name") String name) {
+        return "Hello " + name;
     }
 }
